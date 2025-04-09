1d0d589ab62476e92b2b7e5ea59dd779549a0c7e
Luděk Reif <luipenox@gmail.com>
2025-04-09 21:27:36 +0200

    fix toggle reset

--- functions.py
+++ functions.py
@@ -3,6 +3,11 @@ import streamlit as st
 def load_tasks(header, tasks):
     st.title(header)
 
+    for state in ("hint_toggle_state", "solution_toggle_state", "demonstration_toggle_state"):
+        if state not in st.session_state:
+            st.session_state[state] = False
+
+
     def reset_toggle():
         st.session_state.hint_toggle_state = False
         st.session_state.solution_toggle_state = False
@@ -16,11 +21,11 @@ def load_tasks(header, tasks):
     st.divider()
     _, left_column, middle_column, right_column, _ = st.columns([1, 2, 2, 2, 1])
     with left_column:
-            show_hint = st.toggle("nápověda", value=st.session_state.toggle_state, key="hint_toggle_state")
+            show_hint = st.toggle("nápověda", value=st.session_state.hint_toggle_state, key="hint_toggle_state")
     with middle_column:
-        show_solution = st.toggle("řešení", value=st.session_state.toggle_state, key="solution_toggle_state")
+        show_solution = st.toggle("řešení", value=st.session_state.solution_toggle_state, key="solution_toggle_state")
     with right_column:
-        show_demonstration = st.toggle("ukázka", value=st.session_state.toggle_state, key="demonstration_toggle_state")
+        show_demonstration = st.toggle("ukázka", value=st.session_state.demonstration_toggle_state, key="demonstration_toggle_state")
     st.divider()
 
     if show_hint:
